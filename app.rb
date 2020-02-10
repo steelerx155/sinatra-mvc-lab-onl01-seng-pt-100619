85808e14b94f871130092d590645dff4cb8ad0f8
Randy Steele <steelerx155@gmail.com>
2020-02-10 21:38:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
   
-   post '/piglatinize' do
+   post "/piglatinize" do
     pl = PigLatinizer.new
     @piglatin = pl.piglatinize(params[:user_phrase])
     erb :results
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,8 +1,30 @@
 class PigLatinizer
   
   def piglatinize(words)
-       words.split(" ").length == 1 ? piglatinize_word(words) : piglatinize_sentence(words)
+      words.split(" ").length == 1 ? piglatinize_word(words) : piglatinize_sentence(words)
   end
   
   private
+  
+   def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+  
+    def piglatinize_word(word)
+    if !consonant?(word[0])
+      word = word + "w"
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+  
 end
